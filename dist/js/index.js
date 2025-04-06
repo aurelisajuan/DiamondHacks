8acca8620e492381c6e7f69a3c1600da5368e88e
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-04-05 20:16:00 -0700

    working tailwind

---
+++ dist/js/index.js
@@ -0,0 +1,1 @@
+import{c as t,j as n,R as e}from"../assets/index-b55d538e.js";const s=()=>n.jsx("div",{children:n.jsx("h1",{children:"Thanks for Installing!"})}),o=document.getElementById("onInstalled"),r=t(o);r.render(n.jsx(e.StrictMode,{children:n.jsx(s,{})}));
